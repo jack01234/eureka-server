e3eb35438c94ba5d9a4c68e1cff3e279111a3c48
liwenming <wenming_li@xinyan.com>
2019-04-10 21:57:32 +0800

    注册中心

---
+++ src/main/java/com/wmli/eureka/Application.java
@@ -0,0 +1,18 @@
+package com.wmli.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 启动类
+ *
+ * @author yingmuhuadao
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
